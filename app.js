b828609d2adb78956544aa7c931c503da8d10876
amberamigo <himanshu180599@gmail.com>
2020-12-31 18:02:15 +0530

    added autentication

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-//jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const app = express();
+const _ = require("lodash");
+
+
 
 mongoose.connect("mongodb+srv://amigo_blog:Test123@cluster0.dbkp6.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.set("useCreateIndex", true);
 const postSchema = {
   title : String,
   content : String
 };
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret : "our little secret.",
+  resave : false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/////////////////google OAuth ///////////////////////
+const userSchema = new mongoose.Schema ({
+  email : String, 
+  password : String,
+  googleId : String
+});
+
+userSchema.plugin(passportLocalMongoose); 
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+/////////////-----------------------------//////////////
 
 const Post = mongoose.model("Post", postSchema);
 
@@ -19,30 +80,89 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 
 
 app.get("/", function(req, res){
-  Post.find({}, function(err, posts){
-
-   res.render("home", {
-
-     startingContent: homeStartingContent,
-
-     posts: posts
-
-     });
-
- })
-
-
+  res.render("start");
 });
+////////////////authentication//////////////////////
+
+
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/home");
+  });
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+app.get("/home", function(req, res){
+    if(req.isAuthenticated()){
+      Post.find({}, function(err, posts){
+         res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts
+       });
+    });
+    }else {
+      res.redirect("/login");
+    }
+});
+
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+     User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      })
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+ const user = new User({
+  username : req.body.username,
+  password : req.body.password
+ });
+
+  req.login(user, function(err){
+      if(err){
+        res.redirect("/register");
+      }else{
+       passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      })
+      }
+  });
+});
+
+
+///////////////------------------------------//////////////
+
+
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
@@ -62,13 +182,9 @@ app.post("/compose", function(req, res){
     content: req.body.postBody
   });
 
-   post.save(function(err){
-   if (!err){
-     res.redirect("/");
-   }
+   post.save();
+   res.redirect("/home");
  });
-
-});
 
 
 app.get("/posts/:postId", function(req, res){
