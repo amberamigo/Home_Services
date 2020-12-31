3c7180f69a72eac4cd0feff1f8f739f3688d6306
amberamigo <himanshu180599@gmail.com>
2020-12-31 21:07:01 +0530

    added correct port for heroku

--- app.js
+++ app.js
@@ -64,7 +64,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+     callbackURL: absoluteURI + "/auth/google/secrets",
+      proxy: true 
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
